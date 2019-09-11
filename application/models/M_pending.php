1e3330a5d735d155471c63dc9212e7d032bd0bdc
agie <tiar.agisti@gmail.com>
2019-09-11 11:50:43 +0700

    update bugs receive remark

--- application/models/M_pending.php
+++ application/models/M_pending.php
@@ -33,8 +33,7 @@ class M_pending extends CI_Model
         WHEN ra.approve_status = 3 AND rd.status IS NOT NULL AND ds.status IS NULL AND p.status IS NULL AND b.status IS NULL THEN rd.remark
 				WHEN ra.approve_status = 3 AND rd.status = 2 AND ds.status IS NOT NULL AND p.status IS NULL AND b.status IS NULL THEN ds.remark
         WHEN ra.approve_status = 3 AND rd.status = 2 AND ds.status = 2 AND p.status IS NOT NULL AND b.status IS NULL THEN p.remark
-        WHEN ra.approve_status = 3 AND rd.status = 2 AND ds.status = 2 AND p.status = 2 AND b.status IS NOT NULL THEN b.remark 
-        WHEN ra.approve_status = 3 AND rd.status = 2 AND ds.status = 2 AND p.status = 2 AND b.status = 2 AND r.status IS NOT NULL THEN r.remark ELSE 'Undefined' END 'remark'
+        WHEN ra.approve_status = 3 AND rd.status = 2 AND ds.status = 2 AND p.status = 2 AND b.status IS NOT NULL THEN b.remark ELSE 'Undefined' END 'remark'
         FROM request_details AS rd
         INNER JOIN request_headers AS rh ON rh.request_header_id = rd.request_header_id
         LEFT JOIN request_approves AS ra ON ra.request_header_id = rh.request_header_id
